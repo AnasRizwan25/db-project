11bec16f24a8de5f7fe83323c6d2863eefb62033
AnasRizwan25 <anasgame024@gmail.com>
2025-06-20 02:40:46 +0500

    db complete

--- db/connection.py
+++ db/connection.py
@@ -1,6 +1,6 @@
 import cx_Oracle
 
 def get_connection():
-    dsn = cx_Oracle.makedsn("localhost", 1521, service_name="xe")
-    conn = cx_Oracle.connect(user="taha", password="123", dsn=dsn)
+    dsn = cx_Oracle.makedsn("localhost", "--", service_name="--")
+    conn = cx_Oracle.connect(user="--", password="--", dsn=dsn)
     return conn
